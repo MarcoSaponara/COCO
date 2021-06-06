fa80f9c8fc91ef1793f90ac56ba7ee50f5828850
giuliocerruto <68821738+giuliocerruto@users.noreply.github.com>
2021-06-06 21:07:17 +0200

    Update initialize36.sh

--- 36 layer model/initialize36.sh
+++ 36 layer model/initialize36.sh
@@ -2,14 +2,11 @@
 root=$(pwd)
 mkdir -p CTRL\ Project_36\ Layers
 cd ./CTRL\ Project_36\ Layers
-python -m venv venv 
-#conda create -n venv python=3.6
-#conda activate venv
+python -m venv venv
 source "${root}/CTRL Project_36 Layers/venv/bin/activate"
 ${root}/CTRL\ Project_36\ Layers/venv/bin/python -m pip install --upgrade pip
 (echo "import sys" ; echo "print(sys.prefix)") | python
 pip install tensorflow-gpu==1.14
-#python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.14.0-py3-none-any.whl
 git clone https://github.com/salesforce/ctrl
 pip show tensorflow_estimator
 (echo "import os" ; echo "print(os.listdir('${root}/CTRL Project_36 Layers/venv/lib/python3.6/site-packages/tensorflow_estimator/python/estimator/'))") | python
