b19f2d387ae5e48db02acea1576534b78155f82e
giuliocerruto <68821738+giuliocerruto@users.noreply.github.com>
2021-06-06 21:03:29 +0200

    Update generation36.sh

--- 36 layer model/generation36.sh
+++ 36 layer model/generation36.sh
@@ -3,9 +3,9 @@ root=$(pwd)
 source "${root}/venv/bin/activate"
 (echo "import sys" ; echo "print(sys.prefix)") | python
 cd ./ctrl
-if test "$#" -eq 5; then
-    python generation.py --model_dir seqlen256_36layers_v0.ckpt --temperature $1 --topk $2 --print_once --generate_num $3 --n_sentences $4 --prompt_list $5
-fi
 if test "$#" -eq 6; then
-    python generation.py --model_dir seqlen256_36layers_v0.ckpt --temperature $1 --topk $2 --print_once --generate_num $3 --n_sentences $4 --prompt_list $5 --output_file $6
+    python generation.py --model_dir seqlen256_36layers_v0.ckpt --temperature $1 --topk $2 --print_once --generate_num $3 --n_sentences $4 --nucleus $5 --prompt_list $6
+fi
+if test "$#" -eq 7; then
+    python generation.py --model_dir seqlen256_36layers_v0.ckpt --temperature $1 --topk $2 --print_once --generate_num $3 --n_sentences $4 --nucleus $5 --prompt_list $6 --output_file $7
 fi
